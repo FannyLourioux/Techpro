e3b7bfdc35bef15a0a85bdbb0484f1e30507abed
Romain Vacheret <romain.vacheret@gmail.com>
2021-03-21 18:31:03 +0100

    fix: allow delete requests

--- src/rest/techpro/resources/CorsFilter.java
+++ src/rest/techpro/resources/CorsFilter.java
@@ -42,7 +42,7 @@ public class CorsFilter implements Filter {
 	 
 	        // Authorize (allow) all domains to consume the content
 	        ((HttpServletResponse) servletResponse).addHeader("Access-Control-Allow-Origin", "*");
-	        ((HttpServletResponse) servletResponse).addHeader("Access-Control-Allow-Methods","GET, OPTIONS, HEAD, PUT, POST");
+	        ((HttpServletResponse) servletResponse).addHeader("Access-Control-Allow-Methods","GET, DELETE, OPTIONS, HEAD, PUT, POST");
 	        ((HttpServletResponse) servletResponse).addHeader("Access-Control-Allow-Headers", "Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 
 	 
