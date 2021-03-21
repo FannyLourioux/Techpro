1898262f51684cf6ba8efe4fb6d70ae2e6c2fead
Romain Vacheret <romain.vacheret@gmail.com>
2021-03-21 18:02:41 +0100

    feat: add item deletion

---
+++ src/rest/techpro/resources/ItemResource.java
@@ -0,0 +1,68 @@
+package rest.techpro.resources;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Set;
+import java.util.UUID;
+
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Request;
+import javax.ws.rs.core.UriInfo;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import rest.techpro.dao.ItemDao;
+import rest.techpro.model.Item;
+
+@Path("/item")
+public class ItemResource {
+	private ItemDao dao = ItemDao.getInstance();
+	ObjectMapper mapper = new ObjectMapper();
+	
+	@Context UriInfo uriInfo;
+    @Context Request request;
+	
+	/*
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+	public Set<Item> addItems(JsonNode node) {
+		List<Item> computers = Arrays.asList(mapper.convertValue(node, Item[].class));
+		return dao.addAll(computers);
+	}*/
+	
+    @DELETE
+    @Path("/{param}")
+	public void deleteItems(@PathParam("param") UUID param) {
+    	System.out.println("TOTO");
+    	dao.delete(param);
+    	System.out.println(dao.get(param));
+	}
+    
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+	public String iiii() {
+    	System.out.println("TITI");
+    	return "TITI";
+	}
+	
+	
+    /*
+    @PUT
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+	public Set<Item> updateItems(JsonNode node, @PathParam x) {
+		List<Item> computers = Arrays.asList(mapper.convertValue(node, Item[].class));
+		return dao.updateAll(computers);
+	}*/
+}
