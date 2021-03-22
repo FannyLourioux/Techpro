3dc1db89d831bd9afbf1213fbc64f7782f40de5e
Romain Vacheret <romain.vacheret@gmail.com>
2021-03-22 09:32:43 +0100

    refactor: delete old architecture

--- src/rest/techpro/model/Accessory.java
+++
@@ -1,9 +0,0 @@
-package rest.techpro.model;
-
-public class Accessory extends Product {
-
-	public Accessory(Brand brand, Category category, String label, double price, AccessoryName name) {
-		super(brand, category, label, price);
-	}
-
-}
